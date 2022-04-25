5cde7dcb36aa867e4c4c6a14c189add25c4ac9b7
Paul Sonnentag <paul.sonnentag@gmail.com>
2022-04-25 16:42:35 +0200

    Implement event dispatch

--- prototypes/inspector/src/main/inspector/app.cljs
+++ prototypes/inspector/src/main/inspector/app.cljs
@@ -40,29 +40,77 @@
 
 ; event selectors
 
-(defn get-element-path
+(defn get-element-stack
   ([element]
-   (get-element-path element []))
+   (get-element-stack element '()))
 
-  ([^js element child-path]
+  ([^js element stack]
    (let [dataset (.-dataset element)
-         view-id (.-viewId dataset)
-         db-id (.-dbId dataset)
+         view-id (-> dataset .-viewId js/parseInt)
+         db-id (-> dataset .-dbId js/parseInt)
+         name (-> dataset .-name keyword)
          node (cond-> {}
-                      view-id (assoc :view/id view-id)
-                      db-id (assoc :db/id db-id))
+                      name (assoc :name name)
+                      (not (js/isNaN view-id)) (assoc :view/id view-id)
+                      (not (js/isNaN db-id)) (assoc :db/id db-id))
          root? (= (.-isRoot dataset) "true")]
      (if root?
-       child-path
-       (get-element-path
+       stack
+       (get-element-stack
          (.-parentElement element)
          (if (empty? node)
-           child-path
-           (conj child-path node)))))))
+           stack
+           (conj stack node)))))))
 
 
+(def event-handlers! (atom (sorted-set-by #(-> % :selector count -))))
 
-(def event-handlers (atom []))
+(defn trigger! [triggered-evt stack]
+  (loop [handlers @event-handlers!]
+    (when-let [handler (first handlers)]
+      (let [{:keys [cb selector evt]} handler
+            continue-flag (when (= evt triggered-evt)
+                            (loop [ctx {}
+                                   stack stack
+                                   selector selector]
+                              (let [name (last selector)]
+                                (if name
+                                  (let [node (last stack)]
+                                    (when node
+                                      (if (= (:name node) name)
+                                        (recur (assoc ctx name node)
+                                               (drop-last stack)
+                                               (drop-last selector))
+                                        (recur ctx
+                                               (drop-last stack)
+                                               selector))))
+
+                                  (cb ctx)))))]
+        (when (not (false? continue-flag))
+          (recur (rest handlers)))))))
+
+
+(defn on [evt selector cb]
+  (swap!
+    event-handlers!
+    #(conj % {:evt      evt
+              :selector selector
+              :cb       cb})))
+
+(defn trigger-click! [evt]
+  (let [stack (get-element-stack (.-target evt))]
+    (trigger! :click stack)))
+
+(on :click [:statechart]
+    (fn [ctx]
+      (print "click statechart" ctx)))
+
+(on :click [:statechart :state]
+    (fn [ctx]
+      (print "click state" ctx)
+      false))
+
+; views
 
 (defn state-view []
   (let [ui-id (get-ui-id)]
@@ -70,7 +118,7 @@
       {:display-name "state-view"
        :reagent-render
        (fn [conn name state]
-         [:div.state {:data-view-id ui-id}
+         [:div.state {:data-view-id ui-id :data-name "state"}
           [:h1.state-name name]])})))
 
 (defn statechart-view []
@@ -88,15 +136,13 @@
              (for [[name state] (seq states)]
                ^{:key name} [state-view conn name state])]]))})))
 
-(defn on-click [evt]
-  (print (get-element-path (.-target evt))))
 
 (defn app [conn]
   [:div {:data-is-root true
-         :on-click on-click}
+         :on-click     trigger-click!}
    [statechart-view conn 1]])
 
-(defn init []
+(defn ^:dev/after-load init []
   (dom/render [app conn] (gdom/getElement "root")))
 
 
